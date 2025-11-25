697de9a6b167ad548a93f7f7459431a3f0d98e4a
krola1 <kwl082024@gmail.com>
2025-11-25 11:08:52 +0100

    bang

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,17 @@
 import { useCart } from "../context/CartContext";
 
 export default function ProductCard(props) {
-  const { title, image, price } = props;
+  const { title, image, price, inCart } = props;
   const { addToCart } = useCart();
   return (
     <div className="productCard">
       <h3>{title}</h3> <p>${price}</p>
       <img src={image} alt={title} />
-      <button onClick={() => addToCart(props)}>Add to cart</button>
+      {inCart ? (
+        <button>Remove From cart</button>
+      ) : (
+        <button onClick={() => addToCart(props)}>Add to cart</button>
+      )}
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import { useCart } from "../context/CartContext";
 export default function Cart() {
   const { cart } = useCart();
   return (
-    <div>
+    <div className="productList">
       {cart.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
