b695e857fb371e841d7e0723758cbb4c3ef2a5a6
krola1 <kwl082024@gmail.com>
2025-11-25 10:39:45 +0100

    set up products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,16 @@
+import { useAllProducts } from "../hooks/useProductQuery";
+
 export default function Products() {
+  const { data, isLoading, isError, error } = useAllProducts();
+
+  if (isLoading) return <h1>Loading....</h1>;
+  if (isError) return <h1>Could not load products, error: {error}</h1>;
+
   return (
-    <>
-      <h1>Products</h1>
-    </>
+    <div className="productContainer">
+      {data.map((product) => (
+        <p key={product.id}>{product.title}</p>
+      ))}
+    </div>
   );
 }
