82b37aba0a9e85c7716c62a4d7e5d3c0e705b9aa
krola1 <kwl082024@gmail.com>
2025-11-27 11:21:51 +0100

    created product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,42 @@
+import { useParams } from "react-router-dom";
+import { useProductDetails } from "../hooks/useProductQuery";
 export default function ProductDetails() {
+  const { id } = useParams();
+  const { data: product, error, isLoading, isError } = useProductDetails(id);
+
+  if (isLoading) return <h1>Loading....</h1>;
+  if (isError) return <h1>Could not load data {error}</h1>;
+
   return (
-    <>
-      <h1>ProductDetails</h1>
-    </>
+    <div className="productDetailsWrapper">
+      <div className="productDetailsCard">
+        <div className="productDetailsImageWrapper">
+          <img src={product.image} alt={product.title} />
+        </div>
+
+        <div className="productDetailsInfo">
+          <h1>{product.title}</h1>
+
+          <p className="productDetailsPrice">${product.price}</p>
+
+          <p className="productDetailsRating">
+            ⭐ {product.rating.rate} ({product.rating.count} reviews)
+          </p>
+
+          <p className="productDetailsCategory">
+            Category: <span>{product.category}</span>
+          </p>
+
+          <p className="productDetailsDescription">{product.description}</p>
+
+          <button
+            className="addToCartButton"
+            onClick={() => addToCart(product)}
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
